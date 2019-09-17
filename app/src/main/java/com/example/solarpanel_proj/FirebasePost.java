524dc7e9fbcb7a72b6c7d6b5e79dbd5e010954e3
[ttttuuu11] <[ttttuuu11@naver.com]>
2019-09-17 11:27:46 +0900

    FirebasePost test

--- app/src/main/java/com/example/solarpanel_proj/FirebasePost.java
+++ app/src/main/java/com/example/solarpanel_proj/FirebasePost.java
@@ -1,5 +1,37 @@
 package com.example.solarpanel_proj;
 
+import com.google.firebase.database.Exclude;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.IgnoreExtraProperties;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@IgnoreExtraProperties
 public class FirebasePost {
+
+    public String id;
+    public String name;
+    public Long age;
+
+
+    public FirebasePost(){
+        // Default constructor required for calls to DataSnapshot.getValue(FirebasePost.class)
+    }
+
+    public FirebasePost(String id, String name, Long age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+    @Exclude
+    public Map<String, Object> toMap() {
+        HashMap<String, Object> result = new HashMap<>();
+        result.put("id", id);
+        result.put("name", name);
+        result.put("age", age);
+        return result;
+    }
 
 }
--- app/src/main/java/com/example/solarpanel_proj/MainActivity.java
+++ app/src/main/java/com/example/solarpanel_proj/MainActivity.java
@@ -1,13 +1,37 @@
 package com.example.solarpanel_proj;
 
 import android.os.Bundle;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+import java.util.Map;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    private DatabaseReference mPostReference;
+    String ID="id3";
+    String name="수민";
+    long age=19;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void postUserSignUp(boolean add){
+        mPostReference = FirebaseDatabase.getInstance().getReference();
+        Map<String, Object> childUpdates = new HashMap<>();
+        Map<String, Object> postValues = null;
+        if(add){
+            FirebasePost post = new FirebasePost(ID, name, age);
+            postValues = post.toMap();
+        }
+        childUpdates.put("/id_list/" + ID, postValues);
+        mPostReference.updateChildren(childUpdates);
     }
 }
